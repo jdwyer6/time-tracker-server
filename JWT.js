c20e87e069a6f1bcf82dde41ee45339238df5e42
kegcheck <65734212+kegcheck@users.noreply.github.com>
2022-10-27 15:01:25 -0500

    fix env

--- JWT.js
+++ JWT.js
@@ -1,8 +1,9 @@
-const { sign, verify } = require('jsonwebtoken');
 require("dotenv").config();
+const { sign, verify } = require('jsonwebtoken');
+
 
 const createTokens = (user) => {
-    const accessToken = sign({ username: user.username, id: user._id }, process.env.key);
+    const accessToken = sign({ username: user.username, id: user._id }, `${process.env.TOKEN_SECRET}`);
     return accessToken;
 };
 
@@ -12,7 +13,7 @@ const validateToken = (req, res, next) => {
         return res.status(400).json({error: "User not Authenticated."});
     } 
     try{
-        const validToken = verify(accessToken, process.env.key)
+        const validToken = verify(accessToken, `${process.env.TOKEN_SECRET}`)
         if(validToken){
             req.authenticated = true
             return next()
